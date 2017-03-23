588ead70a33bede73ebd678336729fb3274e08dc
MikeMakes <miguellopezgila@gmail.com>
2017-03-23 19:42:08 +0100

    Ad-Hoc turn on script

--- adhoc/ahon.sh
+++ adhoc/ahon.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" #Directory where this script is
+
+# A slicker error handling routine by William Shotts (www.linuxcommand.org)
+
+PROGNAME=$(basename $0)
+
+error_exit()
+{
+	echo "${PROGNAME}: ${1:-"Unknown Error"}" 1>&2
+	exit 1
+}
+	
+#############################	The script starts here	##############################
+
+
 echo "Backup /etc/network/interfaces as interfaces.backup"
-sudo cp /etc/network/interfaces /etc/network/interfaces.orig
+sudo cp /etc/network/interfaces /etc/network/interfaces.backup || error_exit "$LINENO: Error here!"
 
-read -p "Any key to switch-on the ad-hoc" -n1 -s && echo "/n"		#Wait a key to be pressed && Working in that /n
+#Wait a key to be pressed && Working in that /n
+read -p "Any key to switch-on the ad-hoc" -n1 -s && echo "/n"		
 
-# sudo systemct1 stop dhcpcd.service					#*Workaround below and in ahoff.sh (dhcpcd issue?)
-sudo cp ~/phoebe/adhoc/interfaces.adhoc /etc/network/interfaces		#Change interfaces for interfaces.adhoc
+#Change interfaces for interfaces.adhoc
+sudo cp $DIR/interfaces.adhoc /etc/network/interfaces || error_exit "$LINENO: Error here!"
 
-sudo ifdown wlan0 && sudo ifup wlan0					#Turning off and on the interface
-sudo ifdown wlan0 && sudo ifup wlan0					#*Twice for actually get it working (dhcpcd issue maybe?)
-echo "Ad-Hoc ready" 
+sudo ifdown wlan0 && sudo ifup wlan0	#Turning off and on the interface
+sudo ifdown wlan0 && sudo ifup wlan0	#Twice for actually get it working (dhcpcd issue maybe?)
+echo "Ad-Hoc ready" && exit 0
+
