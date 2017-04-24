97c1f0abc6ad5d26f67d63cff3733d194db9c4ec
MikeMakes <miguellopezgila@gmail.com>
2017-04-24 21:19:31 +0000

    Added simple one-step build script

---
+++ Build/one-step_build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+
+#############################	Description	######################################
+
+#One- Step build
+#This script should install and setup all dependencies in your raspberry pi
+
+
+#############################	Useful things and error handling	##############
+
+#DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" #Directory where this script is (Comment this line if it is not neccesary)
+
+# A slicker error handling routine by William Shotts (www.linuxcommand.org)
+
+PROGNAME=$(basename $0)
+
+error_exit()
+{
+	echo "${PROGNAME}: ${1:-"Unknown Error"}" 1>&2
+	exit 1
+}
+
+
+#############################	The script starts here	##############################
+
+sudo apt-get update
+
+echo "Checking if vlc is installed (and installing it if it''s not), since it would be used as the rtsp client" 
+vlc --version || sudo apt-get install vlc
+
+echo "Checking if picamera library is installed (and installing it if it''s not)" 
+python -c "import picamera" || sudo apt-get install python-picamera
+python3 -c "import picamera" || sudo apt-get install python3-picamera
