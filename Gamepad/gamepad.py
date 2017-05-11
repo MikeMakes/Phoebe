a059ac0f49a35796f06a13183f39d301bf32cc7a
MikeMakes <miguellopezgila@gmail.com>
2017-05-11 03:06:46 +0200

    Gamepad module now accepts the nexts mapping formats; 	Circle = ( int(CircleAxe), int(CircleButton), int(CircleHat) ) 	LeftAnalogJoystick = ( (int(AxeX), int(AxeY), int(AxeZ), ...) , int(LAJButton) , int(LAJHat) ) It is posible to replace any of these ints for "None", the output will be a replacement of None by a list w/ the values of all Axis/Buttons/Hats

--- Gamepad/gamepad.py
+++ Gamepad/gamepad.py
@@ -12,33 +12,50 @@ def whoareyou(n=0):
 def howmanyaxes(n=0):
 	return joysticks[n].get_numaxes()
 
-def get_axes(selection, n=0):
-	out=[None]
-	for i in range(0,len(selection)-1):
-		out.insert(i,joysticks[n].get_axis(selection[i]))
-	out.pop()
-	return out
-
 def get_all_axes(n=0):
 	return [joysticks[n].get_axis(i) for i in range(joysticks[n].get_numaxes())]
+
+def get_axes(selection=None, n=0):
+	if selection:
+		if isinstance(selection[0],tuple):
+			out=[]
+			for i in range(0,len(selection[0])):
+				if selection[0][i] is None:
+					out.insert(i,get_all_axes())
+				else:
+					out.insert(i,joysticks[n].get_axis(selection[0][i]))
+		else
+			out=joysticks[n].get_axis(selection[0])
+	else:
+		out = get_all_axes(n)
+	return out
 
 def howmanybuttons(n=0):
 	return joysticks[n].get_numbuttons()
 
-def get_buttons(selection, n=0):
-	return joysticks[n].get_button(selection[1])
-
 def get_all_buttons(n=0):
 	return [joysticks[n].get_button(i) for i in range(joysticks[n].get_numbuttons())]
+
+def get_buttons(selection=None, n=0):
+	if selection:
+		out = joysticks[n].get_button(selection[-2])
+	else:
+		out=get_all_buttons(n)
+	return out
 
 def howmanyhats(n=0):
 	return joysticks[n].get_numhats()
 
-def get_hats(selection, n=0):
-	return joysticks[n].get_hat(selection[0])
-
 def get_all_hats(n=0):
 	return [joysticks[n].get_hat(i) for i in range(joysticks[n].get_numhats())]
 
+def get_hats(selection=None, n=0):
+	if selection:
+		out=joysticks[n].get_hat(selection[-1])
+	else:
+		out=get_all_axes(n)	
+	return out
+
+
 #create a group of axes, usefull for sticks
-#ls = left stick = ( axe x , axe y , axe z, ... , button/None if there is not button )
+#ls = left stick = ( (axe x , axe y , axe z,...,), button , hat )
