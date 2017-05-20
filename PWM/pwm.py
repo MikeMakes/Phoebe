41f314ddf105947b1e052531b83b31616c0e04ba
MikeMakes <miguellopezgila@gmail.com>
2017-05-20 20:48:07 +0200

    Test for pwm

--- PWM/pwm.py
+++ PWM/pwm.py
@@ -1,7 +1,9 @@
 import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BCM)
 
 def __init__(pin=24,freq=480):
-	GPIO.setmode(GPIO.BCM)  
 	GPIO.setup(pin, GPIO.OUT)
 	return GPIO.PWM(24, 480)
 
@@ -15,4 +17,11 @@ def end(pwm=None):
 		break
 	pwm.stop()
 	GPIO.cleanup()
+	
+if __name__ == "__main__" :
+	pwm=__init__()
+	for i in range(100):
+		start(pwm,i)
+		sleep(15)
+	end(pwm)
 	
