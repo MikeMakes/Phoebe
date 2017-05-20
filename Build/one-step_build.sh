07477e11cd2aad94086dd36fe0b1c0b91069bfae
MikeMakes <miguellopezgila@gmail.com>
2017-05-20 12:24:02 +0200

    Dependencies updated

--- Build/one-step_build.sh
+++ Build/one-step_build.sh
@@ -7,20 +7,6 @@
 #This script should install and setup all dependencies in your raspberry pi
 
 
-#############################	Useful things and error handling	##############
-
-#DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" #Directory where this script is (Comment this line if it is not neccesary)
-
-# A slicker error handling routine by William Shotts (www.linuxcommand.org)
-
-PROGNAME=$(basename $0)
-
-error_exit()
-{
-	echo "${PROGNAME}: ${1:-"Unknown Error"}" 1>&2
-	exit 1
-}
-
 
 #############################	The script starts here	##############################
 
@@ -29,5 +15,8 @@ sudo apt-get update
 echo "Checking if vlc is installed (and installing it if it''s not), since it would be used as the rtsp client" 
 vlc --version || sudo apt-get install vlc
 
-echo "Checking if picamera library is installed (and installing it if it''s not)" 
+echo "Checking evdev library (gamepad input)" 
 python -c "import evdev" || sudo apt-get install python-evdev
+
+echo "Checking socket library (used for TCP connection)"
+python -c "import socket" || sudo apt-get install python-socket
