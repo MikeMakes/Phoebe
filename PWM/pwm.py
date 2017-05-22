0dffa5c8bef521f4487b1313b1d2753873bc640c
MikeMakes <miguellopezgila@gmail.com>
2017-05-22 17:50:04 +0000

    Lack of exceptions!

--- PWM/pwm.py
+++ PWM/pwm.py
@@ -9,22 +9,22 @@ def __init__(pin=24,freq=480):
 
 def start(pwm=None, dc=0):
 	if pwm is None:
-		break
+		return None     #Must be substituted for exception
 	pwm.start(dc)
 
 def changedc(pwm=None, dc=0):
 	if pwm is None:
-		break
+		return None     #Must be substituted for exception
 	pwm.ChangeDutyCicle(dc)
 
 def changefrew(pwm=None, freq=480):
 	if pwm is None:
-		break
+		return None     #Must be substituted for exception
 	pwm.ChangeFrequency(freq)
 
 def end(pwm=None):
 	if pwm is None:
-		break
+		return None     #Must be substituted for exception
 	pwm.stop()
 
 def clean():
