2f4ce22e8f5078210c8c9a88b36a7ae18a502732
MikeMakes <miguellopezgila@gmail.com>
2017-05-22 16:54:19 +0000

    Minor fix

--- TCP/client_example.py
+++ TCP/client_example.py
@@ -2,7 +2,7 @@ from tcp import mysocket
 
 try:
 	client=mysocket()			#New socket instance
-	client.connect("localhost",4200)	#Conect to IP:PORT (locahost for testing)
+	client.connect("192.168.1.192",4200)	#Conect to IP:PORT (locahost for testing)
 	client.send("This is a test")		#Send a string to the server (IT MUST BE A STRING)
 
 finally:
--- TCP/server_example.py
+++ TCP/server_example.py
@@ -2,7 +2,7 @@ from tcp import mysocket
 
 try:
 	server = mysocket()			#New socket instance
-	server.server("localhost",4200)		#Setup server in IP:PORT (localhost for testing)
+	server.server("192.168.1.192",4200)		#Setup server in IP:PORT (localhost for testing)
 	server.accept()				#Accept client connection
 	data = server.receive()			#Receive data from client (ALWAYS A STRING)
 	print "From client: ",data		#Print data (again because mysocket().receive() print it)
