c802273f3e4f7d0177f191e3148402d37928d0bf
Jose Manuel Salvador <elgrandullon115@gmail.com>
2017-05-23 19:11:30 +0200

    PWM prueba de motor

--- PWM/pwmmotor.py
+++ PWM/pwmmotor.py
@@ -1,9 +1,9 @@
 import pwm
 
-motor = GPIO.PWM(24, 480)
-i=5
-iant= 0
-start(motor, 5)
+try:
+	motor = GPIO.PWM(24, 480)
+	i=5
+	start(motor, 5)
 while True:
 	print ("Valor de i:{}".format(i))
 	print ("Esperando nueva i: ")
