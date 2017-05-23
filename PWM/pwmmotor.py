761d1a6c3063a627e350071e75becc0a9df717ad
Jose Manuel Salvador <elgrandullon115@gmail.com>
2017-05-23 18:50:44 +0200

    Cambio para prueba

--- PWM/pwmmotor.py
+++ PWM/pwmmotor.py
@@ -1,21 +1,10 @@
-import RPi.GPIO as GPIO # Cargamos la libreria RPi.GPIO  
-from time import sleep  # cargamos la función sleep del módulo time 
+import pwm
 
-# Probar a poner de nuevo "GPIO.setwarnings(False)" si no funciona asi #
-
-GPIO.setmode(GPIO.BCM)  # Ponemos la Raspberry en modo BCM  
-  
-GPIO.setup(24, GPIO.OUT)  # Ponemos el pin GPIO nº24 como salida para el LED  
-  
-red = GPIO.PWM(24, 480)     # Creamos el objeto 'red' en el pin 24 a 480 Hz 
-  
-def led(a):
-	red.start(a)
-
-led(1)              
-
-# A partir de ahora empezamos a modificar los valores del ciclo de trabajo
-  
-except KeyboardInterrupt:   # Se ha pulsado CTRL+C!!
-    red.stop()              # Detenemos el objeto 'red'
-    GPIO.cleanup()          # Limpiamos los pines GPIO y salimos
+motor = GPIO.PWM(24, 480)
+i=5;
+start(motor, 5)
+print ("Valor de i:{}".format(i))
+changedc(motor, i)
+raw_input()
+	end(motor)
+	clean()
