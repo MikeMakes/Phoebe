8edada331151e79eb257a398475316368589979d
MikeMakes <miguellopezgila@gmail.com>
2017-06-06 09:28:02 +0000

    Some useless code have been deleted. Bullshit Arduino Control have been added.

---
+++ arduControl/BullShitControl1/BullShitControl1.ino
@@ -0,0 +1,28 @@
+#include <Servo.h>
+
+int rm = A0;
+int lm = A1;
+int valueR, valueL;
+
+Servo leftESC, rightESC;
+
+void setup() {
+
+  leftESC.attach(9);  // attached to pin 9 
+  rightESC.attach(10); //attached to pn 10
+}
+
+void loop(){
+ valueR = analogRead(rm);
+ valueL = analogRead(lm);
+ 
+ valueR=map(valueR,0,255,1000,2000);
+ valueL=map(valueL,0,255,1000,2000);
+ 
+ righESC.writeMicroseconds(valueR);
+ leftESC.writeMicroseconds(valueL);
+
+ 
+  
+}
+  
